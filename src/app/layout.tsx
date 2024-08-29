57d0fb1d1201cd6588896fcc522e0b23c8515e88
Stef Altavista <stefanomsc@hotmail.it>
2024-08-29 11:25:18 +0200

    Safari adgustments

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,8 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+
 import "./globals.css";
 import Footer from "@/components/Footer";
 import Background from "@/components/Background";
-
-const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
     title: "S.A.M.",
@@ -18,7 +16,7 @@ export default function RootLayout({
 }>) {
     return (
         <html lang="en">
-            <body className={inter.className}>
+            <body>
                 <Background />
                 {children}
                 <Footer />
--- src/components/Background.tsx
+++ src/components/Background.tsx
@@ -1,15 +1,18 @@
 "use client";
-import React from "react";
 import WaveComplex from "../animations/WaveComplex";
 import WaveSimple from "../animations/WaveSimple";
 import { NextReactP5Wrapper } from "@p5-wrapper/next";
 import gsap from "gsap";
 import { useGSAP } from "@gsap/react";
+import { useEffect, useState } from "react";
+
 export default function Background() {
+    const [popUp, setPopUp] = useState(false);
     useGSAP(() => {
-        gsap.from("#background_animation", { opacity: 0, duration: 2 });
-    });
+        gsap.from("#background_animation", { opacity: 0, duration: 1 });
+    }, []);
     let render;
+
     if (navigator.userAgent.includes("Chrome")) {
         render = <NextReactP5Wrapper sketch={WaveComplex} id="canvas" />;
     } else if (
@@ -18,24 +21,51 @@ export default function Background() {
     ) {
         render = <NextReactP5Wrapper sketch={WaveComplex} id="canvas" />;
     } else {
-        alert(
-            "Welcome to StefanoAltavita.com - Please use a different browser to unlock complex animation"
-        );
+        useEffect(() => {
+            setPopUp(true);
+        }, []);
         render = <NextReactP5Wrapper sketch={WaveSimple} id="canvas" />;
     }
+
     return (
-        <div
-            id="background_animation"
-            style={{
-                height: "100vh",
-                minHeight: "550px",
-                position: "fixed",
-                top: 0,
-                left: 0,
-                zIndex: -2,
-            }}
-        >
-            {render}
-        </div>
+        <>
+            <div
+                id="background_animation"
+                style={{
+                    height: "100vh",
+                    minHeight: "550px",
+                    position: "fixed",
+                    top: 0,
+                    left: 0,
+                    zIndex: -1,
+                }}
+            >
+                {render}
+            </div>
+            {popUp && (
+                <div
+                    id="popUp"
+                    onClick={() => setPopUp(false)}
+                    style={{
+                        boxSizing: "border-box",
+                        zIndex: 5,
+                        display: "flex",
+                        justifyContent: "space-between",
+                        background: "black",
+                        padding: "5px",
+                        width: "100vw",
+                        color: "lime",
+                        fontSize: "10px",
+                        cursor: "pointer",
+                    }}
+                >
+                    <p>
+                        * Please use a different browser to unlock complex
+                        animation
+                    </p>
+                    <p>X</p>
+                </div>
+            )}
+        </>
     );
 }
